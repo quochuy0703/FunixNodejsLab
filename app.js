0111490827371bca55d92b9a9383e8e0a63ef3bb
Huy Mai <quochuy0703@gmail.com>
2022-07-01 21:10:39 +0700

    commit_lab1.5

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   let url = req.url;
+  let method = req.method;
   if (url === "/") {
     let page = `<html>
     <head><title>My first page</title></head>
     <body><form action="/message" method="POST"><input type="text"><button type="submit" name="message">Send</button></form></body>
     </html>`;
     res.write(page);
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode(302);
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
