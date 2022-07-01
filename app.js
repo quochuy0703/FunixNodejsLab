aa2de28deb5f8bb593eaf90e61e537fe045ca964
Huy Mai <quochuy0703@gmail.com>
2022-07-01 20:43:03 +0700

    commit_lab1.3

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-Type", "text/html");
+  let page = `<html>
+    <head><title>My first page</title></head>
+    <body><h1>Hello from my Node.js Server!</h1></body>
+    </html>`;
+  res.write(page);
+  res.end();
 });
 server.listen(3000);
