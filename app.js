7a095259885776372cc633e4ffa585c0e02fb178
Huy Mai <quochuy0703@gmail.com>
2022-07-01 20:59:26 +0700

    commit_lab1.4

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
+  let url = req.url;
+  if (url === "/") {
+    let page = `<html>
+    <head><title>My first page</title></head>
+    <body><form action="/message" method="POST"><input type="text"><button type="submit" name="message">Send</button></form></body>
+    </html>`;
+    res.write(page);
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   let page = `<html>
     <head><title>My first page</title></head>
