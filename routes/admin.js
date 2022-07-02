069304927762643d6dda30fca6e019cdaf6d860f
Huy Mai <quochuy0703@gmail.com>
2022-07-02 20:31:50 +0700

    commit_lab3.1

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,16 @@ const rootDir = require("../utils/path");
 
 const Router = express.Router();
 
+const products = [];
+
 Router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 Router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = Router;
+exports.Router = Router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,10 @@ const rootDir = require("../utils/path");
 
 const Router = express.Router();
 
+const adminData = require("./admin");
+
 Router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
