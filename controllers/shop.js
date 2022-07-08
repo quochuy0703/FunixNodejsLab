527d426eb4cfb55e87024986a55b5b4301411d41
Huy Mai <quochuy0703@gmail.com>
2022-07-08 22:32:25 +0700

    commit_lab5.23

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,32 +40,36 @@ exports.getIndex = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  // let fetchedCart;
-  // req.user
-  //   .getCart((cart) => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then((products) => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
 
-  //     let newQuantity = 1;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
 
-  //     if (product) {
-  //     }
-  //     return Product.findByPk(prodId)
-  //       .then((product) => {
-  //         fetchedCart.addProduct(product, {
-  //           through: { quantity: newQuantity },
-  //         });
-  //       })
-  //       .catch((err) => console.log(err));
-  //   })
-  //   .catch((err) => console.log(err));
-  // res.redirect("/cart");
+      let newQuantity = 1;
+
+      if (product) {
+      }
+      return Product.findByPk(prodId)
+        .then((product) => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity },
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
