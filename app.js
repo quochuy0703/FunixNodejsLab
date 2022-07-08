23b5155c2aa2ce1459f6ea059477d067298d888f
Huy Mai <quochuy0703@gmail.com>
2022-07-08 21:05:11 +0700

    commit_lab5.21

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const shopRoutes = require("./routes/shop");
 const sequelize = require("./utils/database");
 const User = require("./models/user");
 const Product = require("./models/product");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-Item");
 
 const app = express();
 
@@ -35,6 +37,10 @@ app.use("/", errorControllers.get404);
 
 Product.belongsTo(User, { constrants: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
