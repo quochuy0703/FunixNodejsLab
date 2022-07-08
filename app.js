49408cba1f72bc7875e3283237cdde32a06b6f82
Huy Mai <quochuy0703@gmail.com>
2022-07-08 19:23:00 +0700

    commit_lab5.18

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const sequelize = require("./utils/database");
+const User = require("./models/user");
 
 const app = express();
 
@@ -16,6 +17,15 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -26,6 +36,16 @@ sequelize
   .sync()
   .then((result) => {
     // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    } else {
+      return user;
+    }
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
