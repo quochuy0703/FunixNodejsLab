42cc7677e970940a15a1af40346acdd786ac54a5
Huy Mai <quochuy0703@gmail.com>
2022-07-09 14:18:11 +0700

    commit_lab6.2

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://huymq:huymq123456@cluster0-gm4fb.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://huymq:huymq123456@cluster0-gm4fb.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connect Database");
