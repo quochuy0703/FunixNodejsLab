7dc05e5e9639510ee876c3a12848ad1bc1e01454
Huy Mai <quochuy0703@gmail.com>
2022-07-09 14:36:43 +0700

    commit_lab6.5

--- controllers/admin.js
+++ controllers/admin.js
@@ -60,13 +60,9 @@ exports.postAddProducts = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      imageUrl: imageUrl,
-      price: price,
-      description: description,
-    })
+  const product = new Product(title, imageUrl, price, description, imageUrl);
+  product
+    .save()
     .then((result) => {
       console.log("Create product");
       res.redirect("/admin/products");
