057ab82f14552f53a02b4b15ab69a03df2f3e051
Huy Mai <quochuy0703@gmail.com>
2022-07-09 22:00:28 +0700

    commit_lab6.23

--- models/user.js
+++ models/user.js
@@ -77,11 +77,19 @@ class User {
 
   addOrders() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
-      .then((resutl) => {
-        this.cart = [];
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
         return db
           .collection("users")
           .updateOne(
