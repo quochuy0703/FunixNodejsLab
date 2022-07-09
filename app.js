d01b6de5b7a2118c565d240513b9b34b7edce221
Huy Mai <quochuy0703@gmail.com>
2022-07-09 23:25:22 +0700

    commit_lab7.8

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require("./routes/shop");
 
 const mongoose = require("mongoose");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -19,14 +19,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("62c94546b5af378acd8547fc")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("62c9a9ee835e1e681de51f58")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,15 @@ mongoose
   .connect(
     "mongodb+srv://huymq:huymq123456@cluster0-gm4fb.mongodb.net/shop?retryWrites=true&w=majority"
   )
+  // .then((result) => {
+
+  //   const user = new User({
+  //     name: "Max",
+  //     email: "test@gmail.com",
+  //     cart: { items: [] },
+  //   });
+  //   return user.save();
+  // })
   .then((result) => {
     console.log("connect database");
     app.listen(3000);
