c89687457f814912395cbb2c1e7c73327ee2728a
Huy Mai <quochuy0703@gmail.com>
2022-07-10 10:06:34 +0700

    commit_lab7.11

--- models/user.js
+++ models/user.js
@@ -28,6 +28,31 @@ const userSchema = new Schema({
   },
 });
 
+//not use arrow function, because this keyword not in arrow function
+userSchema.methods.addToCart = function (product) {
+  console.log(this.cart);
+  const cartProductIndex = this.cart.items.findIndex(
+    (item) => item.productId.toString() === product._id.toString()
+  );
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: 1,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const getDb = require("../utils/database").getDb;
