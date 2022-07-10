8c26b7f85b8749781cecbc958a8ce57c315405b9
Huy Mai <quochuy0703@gmail.com>
2022-07-10 21:48:32 +0700

    commit_lab8.8

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/auth");
 
 Router.get("/login", authController.getLogin);
 Router.post("/login", authController.postLogin);
+Router.post("/logout", authController.postLogout);
 
 module.exports = Router;
