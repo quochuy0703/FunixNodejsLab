88fd80b22357643056211169796f457ef9660184
Huy Mai <quochuy0703@gmail.com>
2022-07-23 19:28:42 +0700

    commit_lab9.5

--- routes/auth.js
+++ routes/auth.js
@@ -27,6 +27,12 @@ Router.post(
     )
       .isLength({ min: 5 })
       .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match.");
+      }
+      return true;
+    }),
   ],
   authController.postSignup
 );
