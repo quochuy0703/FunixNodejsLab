14e8c7e12b5f3b5a16a3890468b706d063c8abe5
Huy Mai <quochuy0703@gmail.com>
2022-07-23 19:11:06 +0700

    commit_asm9.4

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const Router = express.Router();
 
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -11,15 +11,23 @@ Router.post("/login", authController.postLogin);
 Router.get("/signup", authController.getSigup);
 Router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email!")
-    .custom((value, { req }) => {
-      if (value === "test@test.com") {
-        throw new Error("This email address if forbidden");
-      }
-      return true;
-    }),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email!")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address if forbidden");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "plase enter a password only number and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 Router.post("/logout", authController.postLogout);
