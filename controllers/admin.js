1ab1a8a83f0c197870a3b12d4ab3311c405ce9cc
Huy Mai <quochuy0703@gmail.com>
2022-07-24 20:00:16 +0700

    commit_lab10_6

--- controllers/admin.js
+++ controllers/admin.js
@@ -52,7 +52,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDescription = req.body.description;
 
   const errors = validationResult(req);
@@ -80,7 +80,10 @@ exports.postEditProduct = (req, res, next) => {
     .then((product) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
+      if (image) {
+        product.imageUrl = image.path;
+      }
+
       product.description = updatedDescription;
       return product.save();
     })
@@ -97,11 +100,26 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postAddProducts = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
 
-  console.log("fsfsfsaf" + imageUrl);
+  if (!image) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+      },
+      errorMessage: "Attached file is not an image",
+      validationErrors: [],
+    });
+  }
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -120,8 +138,9 @@ exports.postAddProducts = (req, res, next) => {
       validationErrors: errors.array(),
     });
   }
+
+  const imageUrl = image.path;
   const product = new Product({
-    _id: new mongoose.Types.ObjectId("62c9adc4cc93798efcf97bfc"),
     title: title,
     price: price,
     description: description,
